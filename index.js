d2ac962756465e26e7f9ddd40fb2e4ce24475037
Ionică Bizău <bizauionica@yahoo.com>
2014-06-19 17:44:58 +0300

    Rows generating logic

--- index.js
+++ index.js
@@ -19,19 +19,60 @@ module.exports = function Invoice (options) {
         return self;
     };
 
+    self.convertMainToSecondary = function (input) {
+        return input * options.data.currencyBalance.secondary / options.data.currencyBalance.main;
+    };
+
     self.renderAsHtml = function (ops, callback) {
 
         var template = ops.template || options.config.template
           , path = ops.path || options.config.path
           , tableRowBlock = ops.tableRowBlock || options.config.tableRowBlock
           , tasks = ops.tasks || options.data.tasks
+          , invoiceData = {
+                seller: options.seller
+              , buyer: options.buyer
+              , invoice: options.data.invoice
+              , description_rows: ""
+            }
           ;
 
+        // Read template
         Fs.readFile(template, function (err, templateContent) {
-            Fs.readFile(tableRowBlock, function (err, tableBlockContent) {
+
+            // Read row block
+            Fs.readFile(tableRowBlock, function (err, rowBlockContent) {
+
+                // Render table rows
                 for (var i = 0, cTask; i < tasks.length; ++i) {
                     cTask = tasks[i];
+                    cTask.nrCrt = i + 1;
+                    if (typeof cTask.unitPrice === "number") {
+                        cTask.unitPrice = {
+                            main: cTask.unitPrice
+                          , secondary: self.convertMainToSecondary(cTask.unitPrice)
+                        }
+                    }
+
+                    cTask.amount = {
+                        main: cTask.unitPrice.main * cTask.quantity
+                      , secondary: cTask.unitPrice.secondary * cTask.quantity
+                    };
+
+                    invoiceData.description_rows += Mustache.render(rowBlockContent, cTask);
                 }
+
+                var invoiceHtml = Mustache(templateContent, invoiceData);
+
+                // Output file
+                if (typeof ops.output === "string") {
+                    Fs.writeFile(ops.output, invoiceHtml, function (err, data) {
+                        callback(err, invoiceHtml);
+                    });
+                    return self;
+                }
+
+                callback(null, invoiceHtml);
             });
         });
     };
